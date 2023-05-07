6877b65b758af4748df7667c4c9a2ea511149ddb
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-07 16:36:49 +0300

    ✨feat(main.rs): Add command-line argument parsing The `structopt` crate was used to add command-line argument parsing functionality to the program. The `Cli` struct was created to define the program's available arguments. The `passwords` argument was defined as a required vector of strings that can take in one or more values. Changes made: - The `structopt` crate was added to the program. - The `Cli` struct was created to define the program's available arguments. - The `passwords` argument was defined as a required vector of strings that can take in one or more values. Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "pwned-passwords",
+    about = "Checks whether passwords have been compromised in data breaches."
+)]
+struct Cli {
+    #[structopt(
+        name = "passwords",
+        required = true,
+        min_values = 1,
+        help = "Passwords to check"
+    )]
+    passwords: Vec<String>,
+}
 fn main() {
     println!("Hello, world!");
 }
